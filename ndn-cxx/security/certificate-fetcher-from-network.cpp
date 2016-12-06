df9eb2b9ce10a5c62c2bebe3dcdfa15917748c1f
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2016-12-05 18:34:12 -0800

    [ndnSIM] util: Redirect logger to use the logging facility of NS3
    This commit also includes:
    
    1) Disambiguation of logging component name of CertificateFetcherFromNetwork
    2) Addition of the "ndn-cxx." prefix
    
    Change-Id: Ib345978575104afa436296d2f5a824a39e3a9721

--- ndn-cxx/security/certificate-fetcher-from-network.cpp
+++ ndn-cxx/security/certificate-fetcher-from-network.cpp
@@ -30,7 +30,7 @@ namespace ndn {
 namespace security {
 inline namespace v2 {
 
-NDN_LOG_INIT(ndn.security.CertificateFetcher);
+NDN_LOG_INIT(ndn.security.v2.CertificateFetcher.FromNetwork);
 
 #define NDN_LOG_DEBUG_DEPTH(x) NDN_LOG_DEBUG(std::string(state->getDepth() + 1, '>') << " " << x)
 #define NDN_LOG_TRACE_DEPTH(x) NDN_LOG_TRACE(std::string(state->getDepth() + 1, '>') << " " << x)
