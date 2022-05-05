spirosmastorakis/CFN

[ndnSIM] util: Redirecting the scheduler of ndn-cxx to the scheduler of NS3
Compared to change in previous versions of ndnSIM, this commit includes
much less intrusive changes to the ndn-cxx scheduler, using
ns3::Scheduler only to scheduler a timer event (instead of boost timer
event).

Change-Id: Ie5b2cfe58a4da6d80f9843c922f9983ce332b33f
Refs: #4509

ndn-cxx/util/scheduler.hpp
@@ -26,16 +26,12 @@
 #include "ndn-cxx/detail/cancel-handle.hpp"
 #include "ndn-cxx/util/time.hpp"
 
-#include <boost/system/error_code.hpp>
+#include "ns3/simulator.h"
+
 #include <set>
 
 namespace ndn {
 namespace util {
-
-namespace detail {
-class SteadyTimer;
-} // namespace detail
-
 namespace scheduler {
 
 class Scheduler;
@@ -190,17 +186,14 @@ private:
   scheduleNext();
 
   /** \brief Execute expired events
-   *
-   *  If an event callback throws, the exception is propagated to the thread running the io_service.
-   *  In case there are other expired events, they will be processed in the next invocation.
    */
   void
-  executeEvent(const boost::system::error_code& code);
+  executeEvent();
 
 private:
-  unique_ptr<detail::SteadyTimer> m_timer;
   EventQueue m_queue;
   bool m_isEventExecuting;
+  ndn::optional<ns3::EventId> m_timerEvent;
 
   friend EventId;
 };

glasgow-ipl/ndn-cxx

ndn-cxx/util/scheduler.hpp
@@ -26,15 +26,13 @@
 #include "ndn-cxx/detail/cancel-handle.hpp"
 #include "ndn-cxx/util/time.hpp"
 
-#include <boost/system/error_code.hpp>
+#include "ns3/simulator.h"
+
 #include <set>
 
 namespace ndn {
 
 namespace util {
-namespace detail {
-class SteadyTimer;
-} // namespace detail
 } // namespace util
 
 namespace scheduler {
@@ -160,12 +158,9 @@ private:
   scheduleNext();
 
   /** \brief Execute expired events
-   *
-   *  If an event callback throws, the exception is propagated to the thread running the io_service.
-   *  In case there are other expired events, they will be processed in the next invocation.
    */
   void
-  executeEvent(const boost::system::error_code& code);
+  executeEvent();
 
 private:
   class EventQueueCompare
@@ -178,8 +173,8 @@ private:
   using EventQueue = std::multiset<shared_ptr<EventInfo>, EventQueueCompare>;
   EventQueue m_queue;
 
-  unique_ptr<util::detail::SteadyTimer> m_timer;
   bool m_isEventExecuting = false;
+  std::optional<ns3::EventId> m_timerEvent;
 
   friend EventId;
   friend EventInfo;